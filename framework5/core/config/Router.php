2508257923b2e1330a2d16a9feceb9b7da7d9d38
Tyler Matthews <tmatthewsdev@gmail.com>
2011-04-04 03:59:10 -0400

    Added documentation

--- framework5/core/config/Router.php
+++ framework5/core/config/Router.php
@@ -3,8 +3,13 @@
 namespace Framework5;
 
 /*
+* Framework5 Front Controller Router
+* 	- routes a uri array request to an application controller.
+* 	- one or more Application controllers may be defined.
+* 	- the resolve method should return a package name. ex: 'core.controller.Controller'
+* 	- the router handles the first value in the passed request array
+* 	- all controllers defined must implement the IApplication interface
 * 
-* @author tmatthews (tmatthewsdev@gmail.com)
 */
 
 class Router implements IRouter {
